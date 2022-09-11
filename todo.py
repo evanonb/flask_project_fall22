2183fc96b0486fb3a4ff95c8629717ddba0b67de
evanonb <owenby_evan@columbusstate.edu>
2022-09-11 21:25:11 +0000

    added html page

--- todo.py
+++ todo.py
@@ -1,11 +1,14 @@
 #app.py
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World"
+    return render_template("home.html")
+
+@app.route("/addTodo", method=["POST"])
+def addTodo():
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
